75e254122ebafd2efc2b4ad68934f49790700d7e
LordHunter <huntertmgon@gmail.com>
2023-04-13 10:17:00 -0500

    inicio del dashboard y a la espera de mejoras en la seguridad

--- src/app/pages/components/weather-widget-main/weather-widget-main.component.spec.ts
+++ src/app/pages/components/weather-widget-main/weather-widget-main.component.spec.ts
@@ -10,7 +10,7 @@ describe('WeatherWidgetMainComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         WeatherWidgetMainComponent,
-      ]
+      ],
     })
       .compileComponents();
   }));
